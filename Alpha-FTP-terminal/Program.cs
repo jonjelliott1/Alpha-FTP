b066c9db448ac78d206a5ff4c0717375602c8dd4
jonjelliott@gmail.com <45976921+jonjelliott1@users.noreply.github.com>
2020-02-07 13:40:39 -0500

    At this point my local ftp server is interacting just fine with this code.  I can now move onto building the UI

--- Alpha-FTP-terminal/Program.cs
+++ Alpha-FTP-terminal/Program.cs
@@ -10,8 +10,41 @@ namespace Alpha_FTP_terminal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
-            Console.ReadLine();
+            /* Create Object Instance */
+            ftp ftpClient = new ftp(@"ftp://192.168.1.18/", "ftpuser", "ftpuser");
+
+            ///* Upload a File */
+            //ftpClient.upload("/home/ftpuser/testfile.txt", @"C:\TestFiles\TestFile.txt");
+
+            ///* Download a File */
+            //ftpClient.download("/home/ftpuser/HelloWorld.txt", @"C:\TestFiles\HelloWorld.txt");
+
+            ///* Delete a File */
+            //ftpClient.delete("/home/ftpuser/DeleteMe.txt");
+
+            ///* Rename a File */
+            //ftpClient.rename("/home/ftpuser/RenameMe.txt", "IHaveBeenRenamed.txt");
+
+            ///* Create a New Directory */
+            //ftpClient.createDirectory("/home/ftpuser/CreatedDirectory");
+
+            /* Get the Date/Time a File was Created */
+            string fileDateTime = ftpClient.getFileCreatedDateTime("/home/ftpuser/HelloWorld.txt");
+            Console.WriteLine(fileDateTime);
+
+            /* Get the Size of a File */
+            string fileSize = ftpClient.getFileSize("/home/ftpuser/HelloWorld.txt");
+            Console.WriteLine(fileSize);
+
+            /* Get Contents of a Directory (Names Only) */
+            string[] simpleDirectoryListing = ftpClient.directoryListDetailed("/home/ftpuser");
+            for (int i = 0; i < simpleDirectoryListing.Count(); i++) { Console.WriteLine(simpleDirectoryListing[i]); }
+
+            /* Get Contents of a Directory with Detailed File/Directory Info */
+            string[] detailDirectoryListing = ftpClient.directoryListDetailed("/home/ftpuser");
+            for (int i = 0; i < detailDirectoryListing.Count(); i++) { Console.WriteLine(detailDirectoryListing[i]); }
+            /* Release Resources */
+            ftpClient = null;
         }
     }
 }
